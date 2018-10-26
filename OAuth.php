d4be5eebe2dbb0d5dd9a11204dec33783bfdc6cb
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:18:16 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: Ia9b8771aa6a81f76a7accfa57130aedfdd7cb45a

--- OAuth.php
+++ OAuth.php
@@ -5,12 +5,12 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['MWOAuth'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['MWOAuthAliases'] = __DIR__ . '/frontend/language/MWOAuth.alias.php';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for OAuth extension. ' .
 		'Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
-	die( 'This version of the OAuth extension requires MediaWiki 1.29+' );
+	die( 'This version of the OAuth extension requires MediaWiki 1.32+' );
 }
