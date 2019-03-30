578b2e630ff4e95b88bf13cd838282234a180abf
Lucas Werkmeister <mail@lucaswerkmeister.de>
2019-03-30 14:06:12 +0100

    Add "OAuth CID: $consumerId" tag to patrol log
    When an existing RecentChange is marked as patrolled, the save() method
    is not used, and so our existing hook doesn’t get a chance to add the
    tag for the OAuth consumer if the request was made via OAuth. Subscribe
    to the MarkPatrolled hook, which recently gained a &$tags argument, to
    add the tag here instead.
    
    Bug: T219655
    Change-Id: I2c929f8ef6b5da54bfd75e185f409602470fddd7
    Depends-On: Ie5e6f820bbf399ec639e715afd908f78bf5c8e9a

---
+++ tests/phpunit/api/MWOAuthSessionProviderTest.php
@@ -0,0 +1,93 @@
+<?php
+/**
+ * @section LICENSE
+ * © 2017 Wikimedia Foundation and contributors
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ *
+ * @file
+ */
+
+namespace MediaWiki\Extensions\OAuth;
+
+use MediaWikiTestCase;
+
+/**
+ * @covers \MediaWiki\Extensions\OAuth\MWOAuthSessionProvider
+ * @group OAuth
+ * @license GPL-2.0-or-later
+ */
+class MWOAuthSessionProviderTest extends MediaWikiTestCase {
+
+	protected function setUp() {
+		parent::setUp();
+		// the MWOAuthSessionProvider constructor modifies $wgHooks, stash it
+		global $wgHooks;
+		$this->setMwGlobals( 'wgHooks', $wgHooks );
+	}
+
+	/**
+	 * @dataProvider provideOnMarkPatrolledArguments
+	 */
+	public function testOnMarkPatrolled( $consumerId, $auto, $expectedExtraTag ) {
+		$provider = $this->getMockBuilder( MWOAuthSessionProvider::class )
+			->setMethods( [ 'getPublicConsumerId' ] )
+			->getMock();
+		$provider->expects( $this->once() )
+			->method( 'getPublicConsumerId' )
+			->willReturn( $consumerId );
+
+		$originalTags = [ 'Unrelated tag' ];
+		$tags = $originalTags;
+
+		$provider->onMarkPatrolled( 1, $this->getTestUser()->getUser(), false, $auto, $tags );
+
+		if ( $expectedExtraTag === null ) {
+			$this->assertSame( $originalTags, $tags );
+		} else {
+			$expectedTags = $originalTags;
+			$expectedTags[] = $expectedExtraTag;
+			$this->assertSame( $expectedTags, $tags );
+		}
+	}
+
+	public function provideOnMarkPatrolledArguments() {
+		yield 'no consumer, manually patrolled' => [
+			null,
+			false,
+			null,
+		];
+
+		yield 'no consumer, automatically patrolled' => [
+			null,
+			true,
+			null,
+		];
+
+		yield 'consumer 123, manually patrolled' => [
+			123,
+			false,
+			'OAuth CID: 123',
+		];
+
+		yield 'consumer 1234, automatically patrolled' => [
+			1234,
+			true,
+			'OAuth CID: 1234',
+		];
+	}
+
+}
