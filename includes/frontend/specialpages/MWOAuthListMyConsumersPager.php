5ade546e7473d0de925461bf10f44eef2142eb7a
Umherirrender <umherirrender_de.wp@web.de>
2019-12-11 20:52:20 +0100

    Move specialpage pagers to own class file
    Change-Id: Ib5d96bfdadd1ead83929263041a587a7ed99e635

---
+++ includes/frontend/specialpages/MWOAuthListMyConsumersPager.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace MediaWiki\Extensions\OAuth;
+
+/**
+ * (c) Aaron Schulz 2013, GPL
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
+ * Query to list out consumers
+ *
+ * @TODO: use UserCache
+ */
+class MWOAuthListMyConsumersPager extends \ReverseChronologicalPager {
+	/** @var SpecialMWOAuthConsumerRegistration */
+	public $mForm;
+
+	/** @var array */
+	public $mConds;
+
+	public function __construct( $form, $conds, $centralUserId ) {
+		$this->mForm = $form;
+		$this->mConds = $conds;
+		$this->mConds['oarc_user_id'] = $centralUserId;
+		if ( !$this->getUser()->isAllowed( 'mwoauthviewsuppressed' ) ) {
+			$this->mConds['oarc_deleted'] = 0;
+		}
+
+		$this->mDb = MWOAuthUtils::getCentralDB( DB_REPLICA );
+		parent::__construct();
+
+		# Treat 20 as the default limit, since each entry takes up 5 rows.
+		$urlLimit = $this->mRequest->getInt( 'limit' );
+		$this->mLimit = $urlLimit ?: 20;
+	}
+
+	/**
+	 * @return \Title
+	 */
+	public function getTitle() {
+		return $this->mForm->getFullTitle();
+	}
+
+	/**
+	 * @param \stdClass $row
+	 * @return string
+	 */
+	public function formatRow( $row ) {
+		return $this->mForm->formatRow( $this->mDb, $row );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStartBody() {
+		if ( $this->getNumRows() ) {
+			return '<ul>';
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEndBody() {
+		if ( $this->getNumRows() ) {
+			return '</ul>';
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getQueryInfo() {
+		return [
+			'tables' => [ 'oauth_registered_consumer' ],
+			'fields' => [ '*' ],
+			'conds'  => $this->mConds
+		];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getIndexField() {
+		return 'oarc_stage_timestamp';
+	}
+}
---
+++ includes/frontend/specialpages/MWOAuthManageMyGrantsPager.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace MediaWiki\Extensions\OAuth;
+
+/**
+ * (c) Aaron Schulz 2013, GPL
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * http://www.gnu.org/copyleft/gpl.html
+ */
+
+/**
+ * Query to list out consumers that have an access token for this user
+ *
+ * @TODO: use UserCache
+ */
+class MWOAuthManageMyGrantsPager extends \ReverseChronologicalPager {
+	public $mForm, $mConds;
+
+	/**
+	 * @param SpecialMWOAuthManageMyGrants $form
+	 * @param array $conds
+	 * @param int $centralUserId
+	 */
+	public function __construct( $form, $conds, $centralUserId ) {
+		$this->mForm = $form;
+		$this->mConds = $conds;
+		$this->mConds[] = 'oaac_consumer_id = oarc_id';
+		$this->mConds['oaac_user_id'] = $centralUserId;
+		if ( !$this->getUser()->isAllowed( 'mwoauthviewsuppressed' ) ) {
+			$this->mConds['oarc_deleted'] = 0;
+		}
+
+		$this->mDb = MWOAuthUtils::getCentralDB( DB_REPLICA );
+		parent::__construct();
+
+		# Treat 20 as the default limit, since each entry takes up 5 rows.
+		$urlLimit = $this->mRequest->getInt( 'limit' );
+		$this->mLimit = $urlLimit ?: 20;
+	}
+
+	/**
+	 * @return \Title
+	 */
+	public function getTitle() {
+		return $this->mForm->getFullTitle();
+	}
+
+	/**
+	 * @param \stdClass $row
+	 * @return string
+	 */
+	public function formatRow( $row ) {
+		return $this->mForm->formatRow( $this->mDb, $row );
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getStartBody() {
+		if ( $this->getNumRows() ) {
+			return '<ul>';
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEndBody() {
+		if ( $this->getNumRows() ) {
+			return '</ul>';
+		} else {
+			return '';
+		}
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getQueryInfo() {
+		return [
+			'tables' => [ 'oauth_accepted_consumer', 'oauth_registered_consumer' ],
+			'fields' => [ '*' ],
+			'conds'  => $this->mConds
+		];
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getIndexField() {
+		return 'oaac_consumer_id';
+	}
+}
