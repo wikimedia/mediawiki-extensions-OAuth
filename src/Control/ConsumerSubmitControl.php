a7757935cc4fbaf1a7f5cce4edd793d8ea0f1e10
Reedy <reedy@wikimedia.org>
2021-03-13 18:01:43 +0000

    Make sure length of RSA key will fit in MySQL blob
    Bug: T277380
    Change-Id: I13ff0350a9a0a3cd5ab3e1f82dd0d8d9c13cf9e9

--- src/Control/ConsumerSubmitControl.php
+++ src/Control/ConsumerSubmitControl.php
@@ -50,6 +50,12 @@ class ConsumerSubmitControl extends SubmitControl {
 	protected $dbw;
 
 	/**
+	 * MySQL Blob Size is 2^16 - 1 = 65535 as per "L + 2 bytes, where L < 216" on
+	 * https://dev.mysql.com/doc/refman/8.0/en/storage-requirements.html
+	 */
+	private const BLOB_SIZE = 65535;
+
+	/**
 	 * @param \IContextSource $context
 	 * @param array $params
 	 * @param DBConnRef $dbw Result of MWOAuthUtils::getCentralDB( DB_MASTER )
@@ -63,6 +69,9 @@ class ConsumerSubmitControl extends SubmitControl {
 		$validateRsaKey = function ( $s ) {
 			if ( trim( $s ) === '' ) {
 				return true;
+			}
+			if ( strlen( $s ) > self::BLOB_SIZE ) {
+				return false;
 			}
 			$key = openssl_pkey_get_public( $s );
 			if ( $key === false ) {
