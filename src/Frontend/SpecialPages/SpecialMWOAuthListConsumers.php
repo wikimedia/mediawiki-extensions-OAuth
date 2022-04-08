c196e8362c5300ab073ac47cc88303d9a8857c36
Reedy <reedy@wikimedia.org>
2022-04-09 00:02:44 +0100

    OAuthListConsumers: Only show manage link if user has the right
    Bug: T305761
    Change-Id: I0069c89f99acf4218ace9e7e62b22e7818aff264
    Follows-Up: I9033aa188b0c418c0c16c15e53cbded963818a18

--- src/Frontend/SpecialPages/SpecialMWOAuthListConsumers.php
+++ src/Frontend/SpecialPages/SpecialMWOAuthListConsumers.php
@@ -237,7 +237,7 @@ class SpecialMWOAuthListConsumers extends \SpecialPage {
 			[],
 			$this->getRequest()->getValues( 'name', 'publisher', 'stage' )
 		);
-		if ( !$permMgr->userHasRight( $this->getUser(), 'mwoauthmanageconsumer' ) ) {
+		if ( $permMgr->userHasRight( $this->getUser(), 'mwoauthmanageconsumer' ) ) {
 			$links[] = $this->getLinkRenderer()->makeKnownLink(
 				SpecialPage::getTitleFor( 'OAuthManageConsumers', $cmrKey ),
 				$this->msg( 'mwoauthmanageconsumers-review' )->text()
