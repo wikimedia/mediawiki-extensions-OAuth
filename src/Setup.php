696430d75968bbec9a135f3ed746f229ba0d773a
Reedy <reedy@wikimedia.org>
2022-08-22 18:34:50 +0100

    Move Setup hooks to HookHandler
    Bug: T315908
    Change-Id: I0428115a184c7932816d42e97129f272eeccd41e

--- src/Setup.php
+++ src/Setup.php
@@ -3,30 +3,35 @@
 namespace MediaWiki\Extension\OAuth;
 
 use MediaWiki\Extension\OAuth\Backend\Utils;
+use MediaWiki\Hook\TestCanonicalRedirectHook;
+use OutputPage;
+use RequestContext;
+use Title;
+use WebRequest;
 
 /**
  * Class containing hooked functions for an OAuth environment
  */
-class Setup {
+class Setup implements TestCanonicalRedirectHook {
 	/**
 	 * Prevent CentralAuth from issuing centralauthtokens if we have
 	 * OAuth headers in this request.
 	 * @return bool
 	 */
 	public static function onCentralAuthAbortCentralAuthToken() {
-		$request = \RequestContext::getMain()->getRequest();
+		$request = RequestContext::getMain()->getRequest();
 		return !self::isOAuthRequest( $request );
 	}
 
 	/**
 	 * Prevent redirects to canonical titles, since that's not what the OAuth
 	 * request signed.
-	 * @param \WebRequest $request
-	 * @param \Title $title
-	 * @param \OutputPage $output
+	 * @param WebRequest $request
+	 * @param Title $title
+	 * @param OutputPage $output
 	 * @return bool
 	 */
-	public static function onTestCanonicalRedirect( $request, $title, $output ) {
+	public function onTestCanonicalRedirect( $request, $title, $output ) {
 		return !self::isOAuthRequest( $request );
 	}
 
