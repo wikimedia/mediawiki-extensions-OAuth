45b84014e6847f69ae9e2a81f9ff0b16a18610c5
Gergő Tisza <tgr.huwiki@gmail.com>
2023-10-22 14:07:03 -0700

    Link to user's global accounts in consumer proposal log
    This is convenient for quickly checking whether the user is
    trusted (has advanced privileges or a high edit count on some wiki).
    
    Bug: T103587
    Depends-On: Ic5c4384d230d2837efbf22b97a278573f075f4ad
    Change-Id: Ife8e914205887356da1482b147cf59cf92334cc4

--- src/Frontend/OAuthLogFormatter.php
+++ src/Frontend/OAuthLogFormatter.php
@@ -2,23 +2,33 @@
 
 namespace MediaWiki\Extension\OAuth\Frontend;
 
+use ExtensionRegistry;
 use LogEntry;
 use LogFormatter;
+use MediaWiki\Linker\Linker;
 use MediaWiki\Linker\LinkRenderer;
 use MediaWiki\MediaWikiServices;
 use MediaWiki\Title\Title;
+use MediaWiki\Title\TitleFactory;
+use MediaWiki\User\UserEditTracker;
+use MediaWiki\User\UserIdentity;
 use Message;
 
 /**
  * Formatter for OAuth log events
  */
 class OAuthLogFormatter extends LogFormatter {
-	/** @var LinkRenderer */
-	protected $linkRenderer;
+	protected ExtensionRegistry $extensionRegistry;
+	protected LinkRenderer $linkRenderer;
+	protected TitleFactory $titleFactory;
+	protected UserEditTracker $userEditTracker;
 
 	protected function __construct( LogEntry $entry ) {
 		parent::__construct( $entry );
+		$this->extensionRegistry = ExtensionRegistry::getInstance();
 		$this->linkRenderer = MediaWikiServices::getInstance()->getLinkRenderer();
+		$this->titleFactory = MediaWikiServices::getInstance()->getTitleFactory();
+		$this->userEditTracker = MediaWikiServices::getInstance()->getUserEditTracker();
 	}
 
 	protected function getMessageParameters() {
@@ -37,4 +47,46 @@ class OAuthLogFormatter extends LogFormatter {
 			return Message::rawParam( $this->linkRenderer->makeLink( $title, $consumerKey ) );
 		}
 	}
+
+	/**
+	 * Add a link to the user's global account list to make review more convenient.
+	 * @param UserIdentity $user
+	 * @param int $toolFlags
+	 * @return string
+	 */
+	protected function makeUserLink( UserIdentity $user, $toolFlags = 0 ) {
+		// Only add custom links if a new consumer is being proposed and we can show tool links.
+		if ( $this->entry->getSubtype() !== 'propose'
+			|| $this->plaintext
+			|| !$this->linkFlood
+			|| !$this->extensionRegistry->isLoaded( 'CentralAuth' )
+		) {
+			return parent::makeUserLink( $user, $toolFlags );
+		}
+
+		$userLink = Linker::userLink(
+			$user->getId(),
+			$user->getName()
+		);
+		$this->userEditTracker = MediaWikiServices::getInstance()->getUserEditTracker();
+		$editCount = $user->getId()
+			? $this->userEditTracker->getUserEditCount( $user )
+			: null;
+
+		$toolLinkArray = Linker::userToolLinkArray(
+			$user->getId(),
+			$user->getName(),
+			true,
+			$toolFlags,
+			$editCount
+		);
+		$toolLinkArray[] = $this->linkRenderer->makePreloadedLink(
+			$this->titleFactory->newFromTextThrow( 'Special:CentralAuth/' . $user->getName() ),
+			wfMessage( 'mwoauth-centralauth-account-link' )->text(),
+			'mw-usertoollinks-oauth-globalaccount'
+		);
+
+		return $userLink . Linker::renderUserToolLinksArray( $toolLinkArray, false );
+	}
+
 }
