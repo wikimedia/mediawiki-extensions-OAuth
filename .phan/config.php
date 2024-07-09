7fa91b7d483105675b88b071993ab311c5088776
Umherirrender <umherirrender_de.wp@web.de>
2024-07-09 19:29:59 +0200

    build: Enable phan for Lib folder
    Static analyzer can give some hints on the code.
    
    Change-Id: Ic5cd0b07215a5e1c2566e0eb526101e99d420d6c

--- .phan/config.php
+++ .phan/config.php
@@ -13,7 +13,6 @@ $cfg['directory_list'] = array_merge(
 $cfg['exclude_analysis_directory_list'] = array_merge(
 	$cfg['exclude_analysis_directory_list'],
 	[
-		'src/Lib/',
 		'../../extensions/AbuseFilter',
 		'../../extensions/Echo',
 	]
