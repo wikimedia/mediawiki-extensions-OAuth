ceca2f757f9594e4aaa528111617cc1b57258390
Ed Sanders <esanders@wikimedia.org>
2025-02-19 15:58:49 +0000

    ESLint: Update to ES6 and autofix
    Change-Id: If183e788bb88c18fac23936a12ad85372fbe2b11

--- resources/modules/ext.MWOAuth.AuthorizeDialog.js
+++ resources/modules/ext.MWOAuth.AuthorizeDialog.js
@@ -6,21 +6,21 @@
 ( function () {
 	'use strict';
 
-	var mwoauth = {
+	const mwoauth = {
 		init: function () {
-			var $form = $( '#mw-mwoauth-authorize-form' );
+			const $form = $( '#mw-mwoauth-authorize-form' );
 
 			OO.ui.getWindowManager().openWindow( 'message', {
 				message: $form,
 				size: 'medium',
 				actions: []
-			} ).opened.then( function () {
+			} ).opened.then( () => {
 				// Appending the <form> to a <label> makes the whole form a click target
 				// for 'Allow', so move it out of the <label>
 				OO.ui.getWindowManager().getCurrentWindow().text.$element.append( $form );
 			} );
 
-			$form.on( 'submit', function () {
+			$form.on( 'submit', () => {
 				$( '#mw-mwoauth-accept' ).prop( 'disabled', true );
 			} );
 		}
