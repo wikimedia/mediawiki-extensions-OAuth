324f23806d245d2dc20bb005f5bbcfa27b7fdf35
Gergő Tisza <tgr.huwiki@gmail.com>
2025-10-26 21:45:30 +0100

    Issue SessionManager-style 'sub' claims for non-owner-only
    Issue SessionManager-style 'sub' claims (where the central user ID
    is prefixed by 'mw:' and the central ID provider name) for
    non-owner-only access tokens.
    
    Follows up If671839c558e which made token validation accept such
    tokens.
    
    Owner-only tokens are not changed - they are not really useful for
    traffic control which was the motivation for this change, and they
    are valid forever so we don't want to introduce a new type of them
    lightly.
    
    Bug: T399199
    Change-Id: I24112f80fd554cec6f28d135448620b2ace84e2f

--- src/Entity/AccessTokenEntity.php
+++ src/Entity/AccessTokenEntity.php
@@ -2,11 +2,16 @@
 
 namespace MediaWiki\Extension\OAuth\Entity;
 
+use DateTimeImmutable;
 use InvalidArgumentException;
+use Lcobucci\JWT\Configuration;
+use Lcobucci\JWT\Signer\Key\InMemory;
+use Lcobucci\JWT\Signer\Key\LocalFileReference;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token;
 use League\OAuth2\Server\CryptKey;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\ScopeEntityInterface;
-use League\OAuth2\Server\Entities\Traits\AccessTokenTrait;
 use League\OAuth2\Server\Entities\Traits\EntityTrait;
 use League\OAuth2\Server\Entities\Traits\TokenEntityTrait;
 use League\OAuth2\Server\Exception\OAuthServerException;
@@ -18,7 +23,6 @@ use MediaWiki\WikiMap\WikiMap;
 use Throwable;
 
 class AccessTokenEntity implements AccessTokenEntityInterface {
-	use AccessTokenTrait;
 	use EntityTrait;
 	use TokenEntityTrait;
 
@@ -33,6 +37,8 @@ class AccessTokenEntity implements AccessTokenEntityInterface {
 	 * @var ConsumerAcceptance|false
 	 */
 	private $approval;
+
+	private Configuration $jwtConfiguration;
 
 	/**
 	 * @param ClientEntity $clientEntity
@@ -77,13 +83,17 @@ class AccessTokenEntity implements AccessTokenEntityInterface {
 		$this->confirmClientUsable();
 	}
 
-	/**
-	 * Get the approval that allows this AT to be created
-	 *
-	 * @return ConsumerAcceptance|false
-	 */
-	public function getApproval() {
-		return $this->approval;
+	public function __toString(): string {
+		return $this->convertToJWT()->toString();
+	}
+
+	/** @inheritDoc */
+	public function setPrivateKey( CryptKey $privateKey ) {
+		$this->setJwtConfiguration( Configuration::forAsymmetricSigner(
+			new Sha256(),
+			LocalFileReference::file( $privateKey->getKeyPath(), $privateKey->getPassPhrase() ?? '' ),
+			InMemory::plainText( '' )
+		) );
 	}
 
 	/**
@@ -97,12 +107,30 @@ class AccessTokenEntity implements AccessTokenEntityInterface {
 	}
 
 	/**
+	 * Get the approval that allows this AT to be created
+	 *
+	 * @return ConsumerAcceptance|false
+	 */
+	public function getApproval() {
+		return $this->approval;
+	}
+
+	/**
 	 * Get the client that the token was issued to.
 	 *
 	 * @return ClientEntity
 	 */
 	public function getClient() {
 		return $this->client;
+	}
+
+	public function isOwnerOnly(): bool {
+		return $this->getClient()->getOwnerOnly();
+	}
+
+	/** @internal Exposed for tests only. */
+	public function setJwtConfiguration( Configuration $configuration ): void {
+		$this->jwtConfiguration = $configuration;
 	}
 
 	/**
@@ -154,6 +182,57 @@ class AccessTokenEntity implements AccessTokenEntityInterface {
 				'Client ' . $this->getClient()->getIdentifier() .
 				' is not usable by user with ID ' . $user->getId()
 			);
+		}
+	}
+
+	private function convertToJWT(): Token {
+		// This is a copy of league/oauth-server's AccessTokenTrait, except we use a different
+		// 'sub' claim (relatedTo()).
+
+		$builder = $this->jwtConfiguration->builder()
+			->permittedFor( $this->getClient()->getIdentifier() )
+			->identifiedBy( $this->getIdentifier() )
+			->issuedAt( new DateTimeImmutable() )
+			->canOnlyBeUsedAfter( new DateTimeImmutable() )
+			->expiresAt( $this->getExpiryDateTime() )
+			->relatedTo( $this->getUserIdentifierForJwt() );
+
+		if ( $this->getIssuer() ) {
+			$builder->issuedBy( $this->getIssuer() );
+		}
+
+		foreach ( $this->getClaims() as $claim ) {
+			$builder->withClaim( $claim->getName(), $claim->getValue() );
+		}
+
+		return $builder
+			// Set scope claim late to prevent it from being overridden.
+			->withClaim( 'scopes', $this->getScopes() )
+			->getToken( $this->jwtConfiguration->signer(), $this->jwtConfiguration->signingKey() );
+	}
+
+	private function getUserIdentifierForJwt(): string {
+		global $wgOAuth2UsePrefixedSub;
+
+		$centralId = $this->getUserIdentifier();
+		if (
+			// FIXME is this possible? is it the same as T407655?
+			!$centralId
+			// Owner-only access tokens are valid forever, which makes them impractical for some of the
+			// things other JWTs are used for, and having a different structure helps to differentiate them.
+			// Also, since they are valid forever, and the JWT format might change in the future,
+			// we want to minimize the number of legacy formats we'll have to support forever, so we
+			// go with the already-established legacy format where 'sub' is just a user ID.
+			|| $this->isOwnerOnly()
+			// Temporary feature flag in case something needs to be adapted to the change in the JWT.
+			|| !$wgOAuth2UsePrefixedSub
+		) {
+			return (string)$centralId;
+		} else {
+			// Short-lived non-owner-only access token. Use the new 'sub' format to match
+			// SessionManager::getJwtData().
+			$lookupScope = Utils::getCentralIdLookup()->getScope();
+			return "mw:$lookupScope:$centralId";
 		}
 	}
 
--- tests/phpunit/Entity/AccessTokenEntityTest.php
+++ tests/phpunit/Entity/AccessTokenEntityTest.php
@@ -2,10 +2,15 @@
 
 namespace MediaWiki\Extension\OAuth\Tests\Entity;
 
+use DateInterval;
+use DateTimeImmutable;
+use Lcobucci\JWT\Configuration;
 use League\OAuth2\Server\Entities\ScopeEntityInterface;
 use MediaWiki\Extension\OAuth\Entity\AccessTokenEntity;
 use MediaWiki\Extension\OAuth\Entity\ClaimEntity;
+use MediaWiki\Extension\OAuth\Entity\ClientEntity;
 use MediaWiki\Extension\OAuth\Entity\ScopeEntity;
+use MediaWiki\WikiMap\WikiMap;
 use MediaWikiIntegrationTestCase;
 
 /**
@@ -74,5 +79,39 @@ class AccessTokenEntityTest extends MediaWikiIntegrationTestCase {
 		$this->assertSame( 'dummy', $accessToken->getIssuer() );
 		$accessToken->setIssuer( 'new_dummy' );
 		$this->assertSame( 'new_dummy', $accessToken->getIssuer() );
+	}
+
+	public function testJwt() {
+		$getSub = static function ( ClientEntity $clientEntity, ?int $userId = -1 ) {
+			if ( $userId === -1 ) {
+				$userId = $clientEntity->getUserId();
+			}
+			$accessTokenEntity = new AccessTokenEntity( $clientEntity, [], 'dummy:', $userId );
+			$accessTokenEntity->setIdentifier( 'abcd' );
+			$accessTokenEntity->setExpiryDateTime( ( new DateTimeImmutable() )->add(
+				new DateInterval( 'P1000Y' )
+			) );
+			$accessTokenEntity->setJwtConfiguration( Configuration::forUnsecuredSigner() );
+			$jwt = (string)$accessTokenEntity;
+			$claims = json_decode( base64_decode( explode( '.', $jwt )[1] ), true );
+			return $claims['sub'];
+		};
+		$wikiId = WikiMap::getCurrentWikiId();
+
+		$this->overrideConfigValue( 'OAuth2UsePrefixedSub', false );
+		$clientEntity = MockClientEntity::newMock( $this->getTestUser()->getUser() );
+		$this->assertSame( (string)$clientEntity->getUserId(), $getSub( $clientEntity ) );
+
+		$this->overrideConfigValue( 'OAuth2UsePrefixedSub', true );
+		$this->assertSame( "mw:local:$wikiId:" . $clientEntity->getUserId(), $getSub( $clientEntity ) );
+
+		$this->assertSame( '0', $getSub( $clientEntity, 0 ) );
+		$this->assertSame( '', $getSub( $clientEntity, null ) );
+
+		$clientEntity = MockClientEntity::newMock( $this->getTestUser()->getUser(), [ 'userId' => 0 ] );
+		$this->assertSame( '0', $getSub( $clientEntity ) );
+
+		$clientEntity = MockClientEntity::newMock( $this->getTestUser()->getUser(), [ 'ownerOnly' => true ] );
+		$this->assertSame( (string)$clientEntity->getUserId(), $getSub( $clientEntity ) );
 	}
 }
