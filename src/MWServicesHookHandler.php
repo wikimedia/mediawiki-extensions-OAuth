215c37bc62b0b2d40d276bddc611c3035156020a
Derick Alangi <alangiderick@gmail.com>
2025-11-05 11:44:44 +0300

    Emit deprecation warnings when virtual domain isn't set
    Begin notifying third-parties to set virtual domain now that
    we support it. On a wiki-farm setup, users can point OAuth to
    the shared DB they want the extension to interact with. Note
    that setting `db` to `false` will default to the wiki that
    made the request (local wiki) otherwise this will be a shared
    wiki.
    
    This is actually a followup to I2b5ecfb4b9cf10fdd2e57ce57.
    
    Bug: T409184
    Change-Id: I30ea58a8b8b80e6d8a3efc2689a3a3f770581557

--- src/MWServicesHookHandler.php
+++ src/MWServicesHookHandler.php
@@ -28,6 +28,15 @@ class MWServicesHookHandler implements MediaWikiServicesHook {
 		global $wgMWOAuthCentralWiki, $wgVirtualDomainsMapping;
 
 		if ( $wgMWOAuthCentralWiki && !isset( $wgVirtualDomainsMapping['virtual-oauth'] ) ) {
+			if ( !defined( 'MW_PHPUNIT_TEST' ) && !defined( 'MW_QUIBBLE_CI' ) ) {
+				wfDeprecatedMsg(
+					'Set $wgVirtualDomainsMapping[\'virtual-oauth\'] = [ \'db\' => $wgMWOAuthCentralWiki ];. '
+					. 'OAuth now makes use of virtual database domains.',
+					'1.45',
+					'OAuth'
+				);
+			}
+
 			$wgVirtualDomainsMapping['virtual-oauth'] = [ 'db' => $wgMWOAuthCentralWiki ];
 		}
 	}
