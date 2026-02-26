a87d45169ffa7811f476357490ea10540d75da0c
Gergő Tisza <tgr.huwiki@gmail.com>
2026-02-26 21:58:04 +0100

    Set a JWT cookie for OAuth 1 and OAuth 2 owner-only requests
    OAuth 1 requests do not use JWT, and OAuth 2 owner-only requests
    use a forever-valid JWT (which therefore has outdated information
    about the user). Supplement these with a JWT cookie (gated behind
    a $wgOAuthUseJwtCookie flag).
    
    * A missing / expired / near-expired JWT cookie will
      automatically be updated.
    * A mismatch between the JWT cookie and the access token
      will invalidate the request.
    * An unnecessary but matching JWT cookie (when the provider is
      OAuth 2 non-owner-only) will be ignored. We could unset it,
      but it doesn't seem worth the complexity; and a user might
      be logged in via the session cookie and at the same time
      using a JS-based OAuth 2 app, in which case we should
      probably avoid the two sessions getting into a cookie
      unsetting / recreating fight. (If the two sessions are for
      different users, one will break, but that seems like an
      uncommon enough scenario that we can accept that.)
    
    OAuth sessions normally do not involve persisting the session
    (which for other kinds of sessions is a combination of setting
    cookies + writing to the session backend). We do want to set a
    JWT cookie, but we don't want to write to the backend; access
    tokens are self-contained, it would just be pointless extra load.
    So we use SessionProvider::sessionWasAttachedToRequest(), added
    for this purpose, rather than SessionProvider::persist() + the
    refresh flag like we do for other providers.
    
    Bug: T417833
    Depends-On: I7345201ca0f92fb0ac8af16a2c6c57c915dbd32a
    Change-Id: I117842ccc68383600cd8d7a1f5fdf5ad345360b1

---
+++ src/OAuthConfigNames.php
@@ -0,0 +1,8 @@
+<?php
+
+// phpcs:disable Generic.NamingConventions.UpperCaseConstantName.ClassConstantNotUpperCase
+namespace MediaWiki\Extension\OAuth;
+
+class OAuthConfigNames {
+	public const OAuthUseJwtCookie = 'OAuthUseJwtCookie';
+}
