a750632f6b0e922471ca7ca66ee7737729564633
Gergő Tisza <tgr.huwiki@gmail.com>
2026-03-05 13:38:45 +0100

    Set 'sub' JWT field in client credentials access tokens
    ...without making those access tokens non-anonymous (something we
    might want to do later, but that requires more discussion).
    The 'sub' field is used in Wikimedia edge infrastructrue for rate
    limiting bucketing so an empty 'sub' is a problem.
    
    Bug: T417278
    Change-Id: I5d41107d5e2e3f8221da91801d5230f44efe3439

--- src/AuthorizationProvider/Grant/ClientCredentialsGrantWithCustomClaims.php
+++ src/AuthorizationProvider/Grant/ClientCredentialsGrantWithCustomClaims.php
@@ -6,7 +6,11 @@ use DateInterval;
 use League\OAuth2\Server\Entities\AccessTokenEntityInterface;
 use League\OAuth2\Server\Entities\ClientEntityInterface;
 use League\OAuth2\Server\Grant\ClientCredentialsGrant;
+use LogicException;
 use MediaWiki\Extension\OAuth\AuthorizationProvider\GrantWithCustomClaims;
+use MediaWiki\Extension\OAuth\Entity\AccessTokenEntity;
+use MediaWiki\Extension\OAuth\Entity\ClientEntity;
+use Wikimedia\Assert\Assert;
 
 class ClientCredentialsGrantWithCustomClaims extends ClientCredentialsGrant {
 
@@ -18,9 +22,24 @@ class ClientCredentialsGrantWithCustomClaims extends ClientCredentialsGrant {
 		?string $userIdentifier,
 		array $scopes = []
 	): AccessTokenEntityInterface {
-		$accessToken = parent::issueAccessToken( $accessTokenTTL, $client, $userIdentifier, $scopes );
+		Assert::parameter( $userIdentifier === null, '$userIdentifier', 'must be null' );
+		if ( !( $client instanceof ClientEntity ) ) {
+			throw new LogicException( 'Unexpected type' );
+		}
 
-		// @phan-suppress-next-line PhanTypeMismatchArgumentSuperType
+		$accessToken = parent::issueAccessToken( $accessTokenTTL, $client, $userIdentifier, $scopes );
+		if ( !( $accessToken instanceof AccessTokenEntity ) ) {
+			throw new LogicException( 'Unexpected type' );
+		}
+
+		// T417278 set user ID so the JWT has a `sub` field and doesn't break rate limiting etc.
+		// that relies on that field. This does *not* result in the user being authenticated
+		// (within MediaWiki at least) - that's based on the oauth2_access_tokens entry having
+		// an acceptance ID, the acceptance ID is loaded in
+		// AccessTokenEntity::setApprovalFromClientScopesUser(), which is called in the
+		// constructor, and the user ID is still unset at that point.
+		$accessToken->setUserIdentifier( (string)$client->getUserId() );
+
 		$this->addCustomClaims( $client, $userIdentifier, $accessToken );
 
 		return $accessToken;
